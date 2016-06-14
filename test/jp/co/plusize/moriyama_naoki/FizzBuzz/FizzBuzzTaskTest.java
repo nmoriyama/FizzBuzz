8910d56b4b79504ae55342970b7d9298c9bb2b4e
moriyama <m.nao1026@gmail.com>
2016-06-14 10:23:58 +0900

    引数5の場合Buzzを返すことように変更

--- test/jp/co/plusize/moriyama_naoki/FizzBuzz/FizzBuzzTaskTest.java
+++ test/jp/co/plusize/moriyama_naoki/FizzBuzz/FizzBuzzTaskTest.java
@@ -27,42 +27,42 @@ public class FizzBuzzTaskTest {
 	}
 	
 	@Test
-	public void 引数2の時出力は正しいか() throws Exception {
+	public void 引数2の場合2を返すこと() throws Exception {
 		assertEquals("2", FizzBuzzTask.FizzBuzz(2));
 	}
 	
 	@Test
-	public void 引数3の時Fizzが出力されるか() throws Exception {
+	public void 引数3の場合Fizzを返すこと() throws Exception {
 		assertEquals("Fizz", FizzBuzzTask.FizzBuzz(3));
 	}
 	
 	@Test
-	public void 引数4の時出力は正しいか() throws Exception {
+	public void 引数4の場合4を返すこと() throws Exception {
 		assertEquals("4", FizzBuzzTask.FizzBuzz(4));
 	}
 	
 	@Test
-	public void 引数5の時Buzzが出力されるか() throws Exception {
+	public void 引数5の場合Buzzを返すこと() throws Exception {
 		assertEquals("Buzz", FizzBuzzTask.FizzBuzz(5));
 	}
 	
 	@Test
-	public void 引数6の時出力は正しいか() throws Exception {
+	public void 引数6の場合Fizzを返すこと() throws Exception {
 		assertEquals("Fizz", FizzBuzzTask.FizzBuzz(6));
 	}
 
 	@Test
-	public void 引数14の時出力は正しいか() throws Exception {
+	public void 引数14の場合14を返すこと() throws Exception {
 		assertEquals("14", FizzBuzzTask.FizzBuzz(14));
 	}
 	
 	@Test
-	public void 引数15の時FizzBuzzが出力されるか() throws Exception {
+	public void 引数15の場合FizzBuzzを返すこと() throws Exception {
 		assertEquals("FizzBuzz", FizzBuzzTask.FizzBuzz(15));
 	}
 	
 	@Test
-	public void 引数16の時出力は正しいか() throws Exception {
+	public void 引数16の場合16を返すこと() throws Exception {
 		assertEquals("16", FizzBuzzTask.FizzBuzz(16));
 	}
 }
