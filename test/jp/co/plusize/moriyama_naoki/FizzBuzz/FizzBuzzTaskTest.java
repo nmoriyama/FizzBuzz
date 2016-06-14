683b34490b9201dfaf3f0d925c5cba7bd47d7425
moriyama <m.nao1026@gmail.com>
2016-06-14 10:14:15 +0900

    メソッド名変更

--- test/jp/co/plusize/moriyama_naoki/FizzBuzz/FizzBuzzTaskTest.java
+++ test/jp/co/plusize/moriyama_naoki/FizzBuzz/FizzBuzzTaskTest.java
@@ -27,42 +27,42 @@ public class FizzBuzzTaskTest {
 	}
 	
 	@Test
-	public void Fizzが出力される前の値は正しいか() throws Exception {
+	public void 引数2の時出力は正しいか() throws Exception {
 		assertEquals("2", FizzBuzzTask.FizzBuzz(2));
 	}
 	
 	@Test
-	public void 倍数が3のためFizzが出力されるか() throws Exception {
+	public void 引数3の時Fizzが出力されるか() throws Exception {
 		assertEquals("Fizz", FizzBuzzTask.FizzBuzz(3));
 	}
 	
 	@Test
-	public void Fizzが出力された後とBuzzが出力される前の値は正しいか() throws Exception {
+	public void 引数4の時出力は正しいか() throws Exception {
 		assertEquals("4", FizzBuzzTask.FizzBuzz(4));
 	}
 	
 	@Test
-	public void 倍数が5のためBuzzが出力されるか() throws Exception {
+	public void 引数5の時Buzzが出力されるか() throws Exception {
 		assertEquals("Buzz", FizzBuzzTask.FizzBuzz(5));
 	}
 	
 	@Test
-	public void Buzzが出力された後の値は正しいか() throws Exception {
+	public void 引数6の時出力は正しいか() throws Exception {
 		assertEquals("Fizz", FizzBuzzTask.FizzBuzz(6));
 	}
 
 	@Test
-	public void FizzBuzzが出力される前の値は正しいか() throws Exception {
+	public void 引数14の時出力は正しいか() throws Exception {
 		assertEquals("14", FizzBuzzTask.FizzBuzz(14));
 	}
 	
 	@Test
-	public void 倍数が3と5のためFizzBuzzが出力されるか() throws Exception {
+	public void 引数15の時FizzBuzzが出力されるか() throws Exception {
 		assertEquals("FizzBuzz", FizzBuzzTask.FizzBuzz(15));
 	}
 	
 	@Test
-	public void FizzBuzzが出力された後の値は正しいか() throws Exception {
+	public void 引数16の時出力は正しいか() throws Exception {
 		assertEquals("16", FizzBuzzTask.FizzBuzz(16));
 	}
 }
